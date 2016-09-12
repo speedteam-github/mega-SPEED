64d28edd2816160e46016c9ff83ef3f5b796d7a2
Makhofgithub <makhofman.1.2.3.4.5@gmail.com>
2016-09-12 19:13:08 +0430

    Create rmsg.lua

---
+++ plugins/rmsg.lua
@@ -0,0 +1,31 @@
+﻿local function history(extra, suc, result) 
+  for i=1, #result do 
+    delete_msg(result[i].id, ok_cb, false) 
+  end 
+  if tonumber(extra.con) == #result then 
+    send_msg(extra.chatid, '"'..#result..'" 🔱پیام اخیر سوپر گروه حذف شد🔱', ok_cb, false) 
+  else 
+    send_msg(extra.chatid, '\n🔱#plus🔱پیام های مورد نظر پاکسازی شدند', ok_cb, false) 
+  end 
+end 
+local function run(msg, matches) 
+  if matches[1] == 'r' and is_owner(msg) then 
+    if msg.to.type == 'channel' then 
+      if tonumber(matches[2]) > 200 or tonumber(matches[2]) < 1 then 
+        return "تعداد بیشتر از 200 مجاز نیست" 
+      end 
+      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]}) 
+    else 
+      return "فقط در سوپرگروه ممکن است" 
+    end 
+  else 
+    return "شما دسترسی ندارید" 
+  end 
+end 
+
+return { 
+    patterns = { 
+        '^[!/#](r) (%d*)$' 
+    }, 
+    run = run 
+} 
